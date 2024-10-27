8c45a40f76e0254ea8d22e6e68b2ff4d9a717305
tdwaf <tylerwaf@gmail.com>
2024-10-27 14:41:03 -0400

    style: update ruff script to align with poetry config

--- scripts/ruff.py
+++ scripts/ruff.py
@@ -5,7 +5,14 @@ import sys
 
 def run_ruff_command(args):
     try:
-        result = subprocess.run(['ruff'] + args, check=True)
+        base_args = [
+            '--select',
+            'F,E,I',
+            '--ignore',
+            'E501',
+        ]
+
+        result = subprocess.run(['ruff'] + args + base_args, check=True)
         sys.exit(result.returncode)
     except subprocess.CalledProcessError as e:
         print(f'Command failed with exit code {e.returncode}')
@@ -13,16 +20,27 @@ def run_ruff_command(args):
 
 
 def format():
-    run_ruff_command(['format', '.'])
+    run_ruff_command(
+        [
+            'format',
+            '.',
+            '--quote-style',
+            'single',
+        ]
+    )
 
 
 def lint():
-    run_ruff_command(['check', '.', '--fix'])
+    run_ruff_command(
+        [
+            'check',
+            '.',
+            '--fix',
+        ]
+    )
 
 
 if __name__ == '__main__':
-    import sys
-
     if len(sys.argv) > 1:
         if sys.argv[1] == 'format':
             format()

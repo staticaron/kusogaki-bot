d4cdd5d93c89452b3ca25d54c7a16163f923a895
tdwaf <tdw.github@fastmail.com>
2025-02-05 08:59:16 -0500

    chore: cleanup un-needed init files

--- kusogaki_bot/shared/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from kusogaki_bot.shared.services.image_preloader import ImagePreloader
-from kusogaki_bot.shared.services.image_service import ImageService, image_service
-
-__all__ = ['ImageService', 'image_service', 'ImagePreloader']

5d676031637b761667643fda8866e0d892dfcced
Awaiz <awaiznot@gmail.com>
2025-02-28 22:51:35 -0500

    style: fix formatting issues in miku cog v3

--- kusogaki_bot/features/miku/cog.py
+++ kusogaki_bot/features/miku/cog.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 from kusogaki_bot.core import BaseCog, KusogakiBot
 
+
 class MikuCog(BaseCog):
     """
     Cog that satisfies the miku needs of the deprived individual
@@ -14,9 +15,10 @@ class MikuCog(BaseCog):
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
-            
+
         if 'miku' in message.content.lower():
             await message.channel.send("I'm thinking miku miku oo ee oo")
+
 
 async def setup(bot: KusogakiBot):
     await bot.add_cog(MikuCog(bot))

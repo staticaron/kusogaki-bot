ee66920b5588c0a44581e7af9f1a12842c7e674e
staticaron <devm19102003@gmail.com>
2025-07-11 00:10:41 +0530

    chore: added new env variables

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-from os import environ, getenv
+from os import getenv
 
 from dotenv import load_dotenv
 
-if 'RAILWAY_ENVIRONMENT' not in environ:
-    load_dotenv()
+load_dotenv()
 
 TOKEN = getenv('TOKEN')
+TEST_TOKEN = getenv('TEST_TOKEN')
 STAFF_ROLE_ID = getenv('STAFF_ROLE_ID')
 AWAIZ_USER_ID = getenv('AWAIZ_USER_ID')
+MONGO_URI = getenv('MONGO_URI')
+DB_NAME = getenv('DB_NAME')

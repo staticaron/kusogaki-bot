b9fa480ed2ee2c9c98f10a3701a0a819e25d79b5
tdwaf <68126416+tdwaf@users.noreply.github.com>
2025-10-13 20:31:25 -0400

    chore: remove railway code from config file

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 from os import environ, getenv
 
-from dotenv import load_dotenv
-
-if 'RAILWAY_ENVIRONMENT' not in environ:
-    load_dotenv()
-
 TOKEN = getenv('TOKEN')
 STAFF_ROLE_ID = getenv('STAFF_ROLE_ID')
 AWAIZ_USER_ID = getenv('AWAIZ_USER_ID')
